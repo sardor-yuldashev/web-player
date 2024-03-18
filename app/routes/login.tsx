6bec39545cc2919c701d02350793bed8294b4ffe
sy <syuldashev8135@gmail.com>
2024-03-18 04:27:29 -0400

    for vercel 4

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import { supabase } from "~/supabase";
 
 export default function LogIn() {
-  const handleGoogleSignup = () => {
-    supabase.auth.signInWithOAuth({ provider: "google" });
+  const [email, setEmail] = useState<string | undefined>(undefined)
+  const [password, setPassword] = useState<string | undefined>(undefined)
+
+  // const handleGoogleSignup = () => {
+  //   supabase.auth.signIn({ provider: "google" });
+  // };
+
+  const handleSignInWithEmail = () => {
+    // supabase.auth.signIn({ provider: "google" });
+    supabase.auth.signInWithPassword({
+      email: email || "",
+      password: password || ""
+    })
   };
 
   return (
     <div>
       <p>Log in to your app</p>
-      <button onClick={handleGoogleSignup}>Log in with Google</button>
+      <input onChange={(e) => setEmail(e.currentTarget.value)} type="email" placeholder="Email" value={email} />
+      <input onChange={(e) => setPassword(e.currentTarget.value)} type="password" placeholder="Password" value={password} />
+      <button onClick={handleSignInWithEmail}>Log in with Google</button>
     </div>
   );
 }
